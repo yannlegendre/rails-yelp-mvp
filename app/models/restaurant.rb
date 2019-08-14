07d3f4b5d672c7903d62f679c440db84b847438f
Yann Legendre <yann.legendre@essec.edu>
2019-08-14 11:59:32 +0200

    validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: ["chinese", "italian", "japanese", "french", "belgian"]
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, :rating, presence: true
+  validates :rating, inclusion: (0..5), numericality: { only_integer: true }
 end
